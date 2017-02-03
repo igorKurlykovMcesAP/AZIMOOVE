35bad644479d8cf5fff90177abbcb9fe007edaed
igorKurlykovMcesAP <igor.kurlykoff2010777@mail.ru>
2017-02-03 12:55:50 +0500

    Add files via upload
    files main of dnn brain

---
+++ Azimoove/Form1.Designer.cs
@@ -0,0 +1,214 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button5 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(3, 28);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.OrangeRed;
+            this.button1.Location = new System.Drawing.Point(30, 68);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(108, 99);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Запуск";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Items.AddRange(new object[] {
+            "-1",
+            "-1",
+            "-1",
+            "-1",
+            "-1",
+            "-1",
+            "-1",
+            "-1",
+            "-1",
+            "-1"});
+            this.listBox1.Location = new System.Drawing.Point(907, 58);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(448, 628);
+            this.listBox1.TabIndex = 3;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(677, 28);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(124, 77);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Выбор";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(677, 111);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(124, 75);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Включить";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(677, 192);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(124, 88);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "Выключить";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(43, 481);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(833, 95);
+            this.pictureBox1.TabIndex = 7;
+            this.pictureBox1.TabStop = false;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(109, 28);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 22);
+            this.textBox2.TabIndex = 8;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(194, 111);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(386, 321);
+            this.richTextBox1.TabIndex = 9;
+            this.richTextBox1.Text = "";
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(194, 64);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(386, 41);
+            this.button5.TabIndex = 10;
+            this.button5.Text = "Текст в числа";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(74, 605);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(46, 17);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "label2";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1367, 712);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Включить";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Azimoove/Form1.cs
@@ -0,0 +1,310 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Threading;
+using System.Net;
+using System.Net.Sockets;
+using NAudio.Wave;
+using NAudio.CoreAudioApi;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+
+
+
+
+
+
+
+
+
+
+
+
+
+   
+       public  WaveIn input;
+        //поток для речи собеседника
+       public WaveOut output;
+        //буфферный поток для передачи через сеть
+        public BufferedWaveProvider bufferStream;
+        //поток для прослушивания входящих сообщений
+    
+        public Form1()
+        {
+            InitializeComponent();
+            input = new WaveIn();
+            //определяем его формат - частота дискретизации 16000 Гц, ширина сэмпла - 16 бит, 1 канал - моно
+            input.WaveFormat = new WaveFormat(16000, 16, 1);
+            //добавляем код обработки нашего голоса, поступающего на микрофон
+            input.DataAvailable += Voice_Input;
+            //создаем поток для прослушивания входящего звука
+            output = new WaveOut();
+            //создаем поток для буферного потока и определяем у него такой же формат как и потока с микрофона
+            bufferStream = new BufferedWaveProvider(new WaveFormat(8000, 16, 1));
+            //привязываем поток входящего звука к буферному потоку
+       
+            
+        }
+        public int n_size = 10;
+        public double[,] data = new double[10,100];
+         public  class Neuron
+        {
+             public bool lockingMemory = false;
+              public double sum = 0;  
+            public const int heigth=100,width=10;
+            public double[,] inputs = new double[width,heigth];
+            public double[,] weight = new double[width, heigth];
+            public double[] outputs = new double[4]; 
+           public  Neuron()
+            {
+
+
+
+
+            }
+           public void Absolute(){
+                sum = 0; 
+               for(int i=0;i<width;i++)
+                   for (int j = 0; j < heigth; j++)
+                   {
+                       sum += inputs[i, j] * weight[i, j];
+
+
+                   }
+              
+           }
+               public double Sign(){
+
+                   return Math.Sign(sum);
+
+
+
+               }
+
+            public double Tanh(){
+
+                   return Math.Tanh(sum);
+
+
+
+               }
+          
+            public void Result(){
+                outputs[0]=sum;
+                outputs[1]=Tanh();
+                outputs[2]=Sign();
+
+
+
+            }
+
+            }
+         public Neuron[] Sum = new Neuron[10];
+         public double error = 0;
+         public void LearningPerseptron() // обучение
+         {
+             label1.Text = "";
+             
+             
+                
+                  for (int i = 0; i < 10; i++)
+                  {
+                      double answer=Convert.ToDouble(listBox1.Items[i]);
+                      Sum[i].Absolute();
+                      Sum[i].Result();
+                      label1.Text += " " + Sum[i].outputs[0].ToString() + " ";
+                      if(Sum[i].lockingMemory==false) 
+                      for (int j = 0; j < n_size; j++)
+                      {
+                          double difference = answer - Sum[i].outputs[0];
+                          error = (0.001 * difference * (Sum[i].inputs[0, j]/ Math.Pow(10, Sum[j].inputs[0, j].ToString().Length - 1)));
+        
+                          Sum[i].weight[0, j] += error;
+                          Sum[i].Absolute();
+                          Sum[i].Result();
+                          if (Math.Abs(answer - Sum[i].outputs[0]) < 0.01 && Sum[i].outputs[0]>0) { if (Sum[i].lockingMemory != true) { Sum[i].lockingMemory = true; MessageBox.Show(i.ToString()); } }
+
+
+
+                      }
+
+                  
+
+
+
+
+
+
+
+             }
+
+
+
+
+
+
+         }
+         public double maximum = 0;
+         public double minimum = 0;
+         public int index = 0;
+         public int lastindex = 0;
+        public void DrawMainStream() {
+            Bitmap mainstream = new Bitmap(600, 50);
+            Graphics g = Graphics.FromImage(mainstream); 
+            for (int i = 0; i < 10; i++) { 
+                if (Sum[i].lockingMemory == true)
+                { Sum[i].Absolute();
+                Sum[i].Result();
+                    if (Sum[i].outputs[0] > maximum) 
+                    { maximum = Sum[i].outputs[0]; } } } minimum = maximum;
+            for (int i = 0; i < 10; i++) { if (Sum[i].lockingMemory == true) { Sum[i].Absolute(); Sum[i].Result(); 
+                if (Sum[i].outputs[0] < minimum && Sum[i].outputs[0] > 0  && Math.Abs(Sum[i].outputs[0]-1)<0.1) { minimum = Sum[i].outputs[0]; index = i; } } } for (int i = 0; i < 10; i++) { g.DrawEllipse(new Pen(Color.Black, 2), 10 + i * 16, 10, 10, 10); if (i == index) { g.FillEllipse(new SolidBrush(Color.Green), 10 + i * 16, 10, 10, 10); lastindex = index; } else if (i != index && Sum[i].outputs[0]>0) { g.FillEllipse(new SolidBrush(Color.Yellow), 10 + i * 16, 10, 10, 10); } else { g.FillEllipse(new SolidBrush(Color.Red), 10 + i * 16, 10, 10, 10); } } g.Dispose(); pictureBox1.Image = mainstream; }
+
+
+
+
+     private void Form1_Load(object sender, EventArgs e)
+     {
+         TempStr = listBox1.Items[0].ToString();
+         for (int i = 0; i < 10; i++)
+         {
+
+             Sum[i] = new Neuron();
+             if (textBox1.Text != "" && textBox2.Text !="")
+             {
+                 Sum[i].inputs[0, 0] = Convert.ToDouble(textBox1.Text);
+                 Sum[i].inputs[0, 1] = Convert.ToDouble(textBox2.Text); 
+
+
+             }            
+             Sum[i].Absolute();
+             Sum[i].Result();
+             label1.Text += " " + Sum[i].outputs[0].ToString() + " ";
+         }
+         Random r = new Random();
+         for (int i = 0; i < 10; i++)
+         {
+             for (int j = 0; j < n_size; j++)
+             {
+                 Sum[i].weight[0, j] = r.NextDouble()+0.01;
+
+
+             }
+
+         }
+
+           
+        }
+
+     private void timer1_Tick(object sender, EventArgs e)
+     {
+         LearningPerseptron();
+         DrawMainStream();
+        
+     }
+
+     private void button1_Click(object sender, EventArgs e)
+     {
+         for (int i = 0; i < 10; i++)
+         {
+
+             if (textBox1.Text != "" && textBox2.Text != "")
+             {
+                 Sum[i].inputs[0, 0] = Convert.ToDouble(textBox1.Text);
+                 Sum[i].inputs[0, 1] = Convert.ToDouble(textBox2.Text); ;
+
+
+             }
+             Sum[i].Absolute();
+             Sum[i].Result();
+            
+         }
+         if (timer1.Enabled == true) timer1.Enabled = false;
+         else timer1.Enabled = true;
+     }
+     public int eCount =0;
+     public int temp = -1, lasttemp=-1;
+     public string TempStr = "";
+     private void button2_Click(object sender, EventArgs e)
+     {
+         eCount=0;
+         temp++;
+         if (temp> 9) temp = 0;
+         if (temp!= lasttemp)
+         {
+             if (lasttemp < 0) lasttemp = 0;
+             listBox1.Items[lasttemp] = TempStr;
+             TempStr= listBox1.Items[temp].ToString();
+
+
+             listBox1.Items[temp] += "!";
+
+             lasttemp = temp;
+
+         }
+
+     }
+
+     private void button3_Click(object sender, EventArgs e)
+     {
+         eCount++;
+         TempStr = eCount.ToString();
+         listBox1.Items[temp] = eCount.ToString();
+           
+     }
+
+     private void button4_Click(object sender, EventArgs e)
+     {
+         eCount--;
+         TempStr = eCount.ToString();
+         listBox1.Items[temp] = eCount.ToString();
+           
+     }
+
+     private void button5_Click(object sender, EventArgs e)
+     {
+         for (int i = 0; i < 100; i++) data[0, i] = 0;
+         bool test = false;
+         string text = "";
+         string outtask="";
+         string[] words = new string[1000];
+         string[] dataWords = {"привет","как","у","тебя","дела","семья","здоровье","настроение","учёба","работа","что","делаешь","зовут","где","живёшь" };
+         text = richTextBox1.Text;
+         int a = 0;
+         for (int i = 0; i < text.Length; i++)
+         {
+
+             if (text[i] == ' ' || text[i] == ',' || text[i] == '.' || text[i] == '*' || text[i] == '&' || text[i] == '^' || text[i] == '!' || text[i] == '?' || text[i] == '>' || text[i] == '<' || text[i] == '#' || text[i] == '$' || text[i] == '@') { if (test != true) a++; test = true; }
+             else { test = false; words[a] += text[i]; }
+
+         }
+         
+
+         for (int i = 0; i < dataWords.Length; i++)
+         {
+             for (int j = 0; j < a+1; j++)
+
+                 if (dataWords[i] == words[j]) { data[0, j] = i; outtask += " :" + data[0, j].ToString();  }
+
+
+         }
+         for (int i = 0; i < 10; i++) { Sum[i].inputs = data; }
+
+        
+         label2.Text =outtask;
+
+     }
+
+     public EventHandler<WaveInEventArgs> Voice_Input { get; set; }
+    }
+    
+
+}
